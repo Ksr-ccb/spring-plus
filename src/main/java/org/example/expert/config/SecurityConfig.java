381afda45292152e4cbe54def03c25662a291694
ksr-ccb <harang4282@gmail.com>
2025-05-02 19:13:20 +0900

    feat(Level2) - Spring Security

--- src/main/java/org/example/expert/config/SecurityConfig.java
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package org.example.expert.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.example.expert.domain.auth.exception.AuthException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -22,19 +22,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
-                //이건 basic auth 사용안한다는 표기인ㄷㅔ REST API 사용중이라 disable 해놓으면됨
+            //이건 basic auth 사용안한다는 표기인ㄷㅔ REST API 사용중이라 disable 해놓으면됨
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-            // [2] CSRF 비활성화 ( REST API + JWT 환경에서는 CSRF 필요 없음 → `csrf().disable()` 붙이면 된다고함. )
+        // [2] CSRF 비활성화 ( REST API + JWT 환경에서는 CSRF 필요 없음 → `csrf().disable()` 붙이면 된다고함. )
                 .csrf(AbstractHttpConfigurer::disable)
-            // [3] 세션 STATELESS 설정 ( JWT는 세션 안 씀 →
-            //  `sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)` 붙이면된다.
+        // [3] 세션 STATELESS 설정 ( JWT는 세션 안 씀 →
+        //  `sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)` 붙이면된다.
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            // [4] URL별 권한 설정
+        // [4] URL별 권한 설정
                 .authorizeHttpRequests(auth-> auth
 
-            // [5] 인증/인가 정책 설정
+        // [5] 인증/인가 정책 설정
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
@@ -42,6 +42,14 @@ public class SecurityConfig {
         // [6] JWT 필터 삽입
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
         // [7] 예외 처리 커스터마이징
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
+                        })
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자 권한이 없습니다.");
+                        })
+                )
         // [8] CORS 설정 (필요 시)
                 .build(); // [9] SecurityFilterChain 반환
     }
