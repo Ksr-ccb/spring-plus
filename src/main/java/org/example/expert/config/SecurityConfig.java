7d446e75a2d60062ce2983a8cf93a1504c86cb95
ksr-ccb <harang4282@gmail.com>
2025-05-07 13:15:11 +0900

    feat(Level2) - Spring Security

--- src/main/java/org/example/expert/config/SecurityConfig.java
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -22,26 +22,30 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
-            //이건 basic auth 사용안한다는 표기인ㄷㅔ REST API 사용중이라 disable 해놓으면됨
+                //이건 basic auth 사용안한다는 표기인ㄷㅔ REST API 사용중이라 disable 해놓으면됨
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-        // [2] CSRF 비활성화 ( REST API + JWT 환경에서는 CSRF 필요 없음 → `csrf().disable()` 붙이면 된다고함. )
+
+                // [2] CSRF 비활성화 ( REST API + JWT 환경에서는 CSRF 필요 없음 → `csrf().disable()` 붙이면 된다고함. )
                 .csrf(AbstractHttpConfigurer::disable)
-        // [3] 세션 STATELESS 설정 ( JWT는 세션 안 씀 →
-        //  `sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)` 붙이면된다.
+
+                // [3] 세션 STATELESS 설정 ( JWT는 세션 안 씀 →
+                //  `sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)` 붙이면된다.
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        // [4] URL별 권한 설정
-                .authorizeHttpRequests(auth-> auth
 
-        // [5] 인증/인가 정책 설정
+                // [4] URL별 권한 설정
+                .authorizeHttpRequests(auth-> auth
+                        // [5] 인증/인가 정책 설정
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-        // [6] JWT 필터 삽입
+
+                // [6] JWT 필터 삽입
                 .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
-        // [7] 예외 처리 커스터마이징
+
+                // [7] 예외 처리 커스터마이징
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "유효하지 않는 JWT 서명입니다.");
@@ -50,7 +54,9 @@ public class SecurityConfig {
                             response.sendError(HttpServletResponse.SC_FORBIDDEN, "관리자 권한이 없습니다.");
                         })
                 )
-        // [8] CORS 설정 (필요 시)
+
+                // [8] CORS 설정 (필요 시)
+
                 .build(); // [9] SecurityFilterChain 반환
     }
 }
